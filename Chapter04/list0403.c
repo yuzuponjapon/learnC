db2ba27fabe52119bbcbfeb49b9d55706cf1cc0e
yuzuponjapon <katpoke@gmail.com>
2021-11-20 22:27:40 +0900

    do while文、次々入力

---
+++ Chapter04/list0403.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(void)
+{
+	int sum = 0;
+	int cnt = 0;
+	int retry;
+
+	do {
+		int t;
+		printf("整数を入力してください。＞");
+		scanf("%d",&t);
+
+		sum = sum + t;
+		cnt = cnt + 1;
+
+		printf("まだ？　yes = 0 : no = 9 ＞");
+		scanf("%d",&retry);
+	} while ( retry == 0 );
+
+	printf("合計は%dで平均は%.2fです。\n",sum,(double)sum/cnt);
+
+	return 0;
+}
