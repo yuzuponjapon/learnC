1b5e5f379b85ddeff4e5f9703d49098505922910
yuzuponjapon <katpoke@gmail.com>
2022-01-08 06:01:22 +0900

    関数を作って入力値を逆順表示

---
+++ Chapter06/list0609.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+/*** 正の整数を読み込んで返す ***/
+int input_num(void)
+{
+	int num;
+	do {
+
+		printf("正の整数を入力してください。＞");
+		scanf("%d",&num);
+		if (num <= 0)
+			puts("正の整数を入力してください。");
+	} while(num <= 0);
+	return num;
+}
+
+/*** 非負の整数を反転して返す ***/
+int rev_int(int num)
+{
+	int tmp = 0;
+	
+	while (num > 0)
+	{
+		tmp = num % 10;
+		tmp *= 10;
+		num /= 10;
+	}
+	return tmp;
+}
+
+/*** 関数呼び出し ***/
+int main(void)
+{
+/*	int nx = input_num(); */
+	printf("反転した値は%dです。\n",rev_int(input_num()));
+	return 0;
+}
