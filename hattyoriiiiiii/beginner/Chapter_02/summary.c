73d263a6404efc76188f0b2b99461cbdfc8d864f
Hattyoriiiiiii <www.tatsuya92@gmail.com>
2022-01-18 06:14:00 +0900

    Chapter02終了

---
+++ hattyoriiiiiii/beginner/Chapter_02/summary.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    int b;
+    double r;
+
+    printf("aとbの値: ");
+    scanf("%d%d", &a, &b);
+
+    printf("a + b = %d\n", a+b);
+    printf("a - b = %d\n", a-b);
+    printf("a * b = %d\n", a*b);
+    printf("a / b = %d\n", a/b);
+    printf("a %% b = %d\n", a%b);
+
+    printf("(a+b)/2 = %d\n", (a+b)/2);
+    printf("平均値は%f\n\n", (double)(a+b)/2);
+
+    printf("半径: ");
+    scanf("%lf", &r);
+
+    printf("半径が%.3fの円の面積は%.3fです。\n", r, 3.14 * r * r);
+
+    return 0;
+}
