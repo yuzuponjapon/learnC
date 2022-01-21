5e31675abac283b91656a087b7d0abce32bf5159
yuzuponjapon <katpoke@gmail.com>
2022-01-22 05:51:46 +0900

    ビット単位論理演算

---
+++ Chapter07/list0706.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int count_bits(unsigned x)
+{
+	int bits = 0;
+	while(x) {
+		if (x & 1U) bits++;
+		x >>= 1;
+	}
+	return bits;
+}
+
+int int_bits(void)
+{
+	return count_bits(~0U);
+}
+
+void print_bits(unsigned x)
+{
+	int i;
+	for(i = int_bits() -1;i >= 0;i--)
+		putchar(((x >> i) & 1U) ? '1':'0');
+}
+
+int main(void)
+{
+	unsigned a,b;
+
+	printf("非負の整数を２つ入力してください。\n");
+	printf("a : ");		scanf("%u",&a);
+	printf("b : "); 	scanf("%u",&b);
+
+	printf("\na      = ");	print_bits(a);
+	printf("\nb      = ");	print_bits(b);
+	printf("\na & b  = ");	print_bits(a & b);
+	printf("\na | b  = ");	print_bits(a | b);
+	printf("\na ^ b  = ");	print_bits(a ^ b);
+	printf("\n~a     = ");	print_bits(~a);
+	printf("\n~b     = ");	print_bits(~b);
+	putchar('\n');
+
+	return 0;
+}
