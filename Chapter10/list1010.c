3116067e17f24df002a6b61d926c55b0c1823999
yuzuponjapon <katpoke@gmail.com>
2022-02-09 06:23:28 +0900

    配列のポインタ

---
+++ Chapter10/list1010.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	int a[5] = {1,2,3,4,5};
+	int *p = a;
+
+	for(i = 0;i < 5;i++) {
+		printf("a[%d] = %d   *(a+%d) = %d   p[%d] = %d   *(p+%d) = %d\n",
+		      i,a[i],      i,*(a+i),     i,p[i],       i,*(p+i));
+	}
+
+	return 0;
+} 
