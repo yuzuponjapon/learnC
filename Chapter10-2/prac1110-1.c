a22add7e454187f4866795f1f2ebece901a87bf5
yuzuponjapon <katpoke@gmail.com>
2022-02-24 06:36:09 +0900

    prac1110-1改

--- Chapter10-2/prac1110-1.c
+++ Chapter10-2/prac1110-1.c
@@ -1,8 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
 /*****************/
 /*** 自作 atoi ***/
 /*****************/
-#include <stdio.h>
-#include <stdlib.h>
+/****************************************
 
 int k_atoi(const char *s)
 {
@@ -14,16 +15,16 @@ int k_atoi(const char *s)
 	int tmp = 0;
 	
 
-	while('0' < s[keta] && s[keta] < '9')  /* 桁数確認 */
-		keta++;
+	while('0' < s[keta] && s[keta] < '9')  * 桁数確認 *
+		keta++
 
 
 
 	int kakerukaisu = keta - 1;
 
-	for( i=0; i < keta; i++ ) {  /* 桁数分ループ */ 
+	for( i=0; i < keta; i++ ) {  * 桁数分ループ *
 		tmp = s[i] - '0';
-		for( j=0; j < kakerukaisu; j++) {  /* *s　掛ける１０のループ */
+		for( j=0; j < kakerukaisu; j++) {  * *s　掛ける１０のループ *
 			tmp = tmp * 10;
 		}
 		result += tmp;		
@@ -32,7 +33,27 @@ int k_atoi(const char *s)
 
 	return result;
 }
-			
+*****************************************/			
+
+/********************/
+/*** 自作 atoi 改 ***/
+/********************/
+
+int k_atoi(const char *s)
+{
+	int sum = 0;
+	while( *s != 0 ) {
+		sum += *s - '0';
+		sum *= 10;
+		s++;
+	}
+	sum /= 10;  
+		
+	return sum;
+}
+
+
+
 
 
 int main(void)
