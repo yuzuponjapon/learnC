6c372a4fdcd84e35c4b5cf2fde40e7245a327036
yuzuponjapon <katpoke@gmail.com>
2022-05-28 17:32:14 +0900

    マスターマインド自作

--- intermediate/Chapter04/list0406.c
+++ intermediate/Chapter04/list0406.c
@@ -4,91 +4,100 @@
 
 
 
-
-
-
-
-
 void opening()
 {
-	puts("■　マスターマインドをしましょう。");
-	puts("■　四つの数字の並びを当てて下さい。");
-	puts("■　同じ数字が複数含まれることはありません。");
-	puts("■　連続して入力してください。");
-	puts("■　スペース文字等を入力しないでください。");
+	printf("マスターマインドをしましょう。\n");
 }
 
 
-void make_ans( int ans[])
+void make_ans(int ans[])
 {
-	int i,j;
 	int tmp;
+	int i,j;
 
 	srand(time(NULL));
 
 	for(i = 0; i < 4; i++) {
 		do {
+
 			tmp = rand() % 10;
 		
 			for(j = 0; j < i; j++) {
-				if( ans[j] == tmp)
+				if(tmp == ans[j])
 					break;
 			}
-		} while(j < i);
+
+		} while( j < i);
 		
 		ans[i] = tmp;
 	}
-}
+}		
 
-
-int human_ans( char h_ans[])
+int input_ans(char ans[])
 {
-	int i;
-	char buf[20];
+	int i,j;
+	char buf[10];
 
-	printf("数字を入力してください。＞");
-	scanf("%s",buf);	
+	printf("input >");
+	scanf("%s",buf);
 
-	if( strlen( buf) != 4)
+	if(strlen(buf) !=4)
 		return 1;
-
 	for(i = 0; i < 4; i++) {
 		if(!isdigit(buf[i]))
 			return 2;
-		
 		for(j = 0; j < i; j++) {
-			if(buf[i] == buf[j]
+			if(buf[i] == buf[j])
 				return 3;
-			else
-				h_ans[i] = buf[i];
 		}
+		ans[i] = buf[i];
 	}
+	return 0;
 }
-
-void disp_err(int flag)
+		
+void disp_err(int e_flag)
 {
-	switch( flag) {
-		case 1:	printf("四ケタで\n");	break;
-		case 2:	printf("数字のみで\n");	break;
-		case 3:	printf("重複しないで\n");	break;
+	switch(e_flag) {
+		case 1:	printf("四ケタで");		break;
+		case 2:	printf("数字で");		break;
+		case 3:	printf("重複しないで");	break;	
 	}
+	printf("入力して\n");
 }
 
-void judge(int c_ans[], char h_ans[], int *blow, int *hit)
+void judge(const int c_ans[], const char h_ans[], int *hit, int *blow)
 {
 	int i,j;
-	
+
+	*hit = *blow = 0;
+/*
+int k;
+printf("judge c_ans = ");
+for(k=0;k<4;k++) {
+	printf("%d",c_ans[k]);
+}
+printf("   h_ans = %s\n",h_ans);
+*/
+
 	for(i = 0; i < 4; i++) {
 		for(j = 0; j < 4; j++) {
-			if(c_ans[i] + '0' == h_ans[j])
+			if(c_ans[i] + '0' == h_ans[j]) {
 				if(i == j)
 					(*hit)++;
 				else
 					(*blow)++;
+			}
 		}
-	}
+	}	
+/*
+printf("judge  hit = %d  blow = %d\n",*hit,*blow);
+*/
 }
-			
+
+
+
+
+
 
 
 
@@ -96,32 +105,43 @@ void judge(int c_ans[], char h_ans[], int *blow, int *hit)
 int main(void)
 {
 
-	int check_flag;
-	int judge_flag;
 	int comp_ans[4];
 	char human_ans[4];
-	int blow,hit;
+	int hit = 0;
+	int blow = 0;
+	int err_flag;
+	int cnt = 0;
 
-	opening();	/* オープニング */
+	/* オープニング */
+	opening();
 
-	make_ans( comp_ans);	/* 解答作成 */
+	/* 解答作成 */
+	make_ans(comp_ans);
+/*
+int i;
+for(i=0;i<4;i++) {
+	printf("%d",comp_ans[i]);
+}
+*/
 
 	do {
+		/* 入力、値確認 */
+		err_flag = input_ans(human_ans);
+
+		if(err_flag != 0)
+			/* 入力エラー表示 */
+			disp_err(err_flag);
+		else {	
+			/* 正解判定 */
+			judge(comp_ans, human_ans, &hit, &blow);
+			printf("hit = %d  blow = %d\n",hit,blow);
+		}
+		cnt++;
+	} while(hit < 4);
+
 	
-		do {
-		
-			check_flag = human_ans( human_ans);	/* 解答入力 入力値妥当チェック */
-			
-			disp_err(check_flag);		/* 入力値エラー表示 */
-        
-		} while(check_flag != 0);
-
-		judge(comp_ans,human_ans,&blow,&hit);	/* 正解判定 */
-
-	} while(judge_flag != 0);
-
-	result();	/* 結果表示 */
-
+	/* 結果表示 */
+	printf("%d回で正解です\n",cnt);
 
 	return 0;
-
+}
